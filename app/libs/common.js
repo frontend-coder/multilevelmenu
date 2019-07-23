f58dd2417238615bde1befa210bafe68b45a2f03
Oleg <frontendercode@gmail.com>
2019-07-23 19:49:08 +0300

    v.0.1.1
    Выполнил настройку работу  выдвигающегося с левой стороны меню, гамбургер остается в правой части окна

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,5 +1,13 @@
-$(document).ready(function () {
 
+
+
+
+
+
+
+
+
+$(document).ready(function () {
 
 	$('.owl-carousel').owlCarousel({
     rtl:true,
@@ -24,9 +32,37 @@ $(document).ready(function () {
 
 
 
+$('button.effect1').on('click',function(){
+  $(this).find('span').toggleClass('active');
+      $(".top_sidemenu").toggleClass("top_sidemenu-active");
+});
 
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
 
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
 
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}
+
+	var accordion = new Accordion($('#accordion'), false);
+});
 
 
 });
